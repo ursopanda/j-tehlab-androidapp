85f0b034fd005d6e1673fde7c4099c6464536857
Emil <e.syundyukov@gmail.com>
2017-09-26 13:22:54 +0300

    Initial commit

---
+++ app/src/main/java/kruts/ir/rudolfs/rudolfsirkruts/MainActivity.java
@@ -0,0 +1,29 @@
+package kruts.ir.rudolfs.rudolfsirkruts;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void spiediens(View view) {
+
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+
+
+//        Intent intent = new Intent(this, Main2Activity.class);
+//        startActivity(intent);
+
+    }
+
+
+}
